fd8beccacc49065ca4f71e67a0f303143f913a1a
Alexander <alex.kononok@gmail.com>
2020-12-27 20:10:25 +0300

    Created main view controller

---
+++ Weather/TodayWeatherViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TodayWeatherViewController.swift
+//  Weather
+//
+//  Created by Alexander Kononok on 12/27/20.
+//
+
+import UIKit
+
+class TodayWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Weather/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Alexander Kononok on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var cityPicker: UIPickerView!
+
+    let citiesArray: [String] = ["Minsk", "Gomel"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "WEATHER"
+
+        cityPicker.dataSource = self
+        cityPicker.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        okButton.layer.cornerRadius = okButton.frame.height / 2
+    }
+
+    @IBAction func okButtonPressed(_ sender: Any) {
+        let todayWeatherStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let todayWeatherViewController = todayWeatherStoryboard.instantiateViewController(
+            identifier: String(describing: TodayWeatherViewController.self))
+            as? TodayWeatherViewController
+    }
+
+}
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return citiesArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return citiesArray[row]
+    }
+
+}
